544c127f7687ee5cbe7ba32b0a2263d8e4dba163
51lv3r258 <jrp8900@gmail.com>
2021-09-26 00:05:04 -0500

    ✨ Use some properties of ListTile

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
   const HomePageTemp({Key? key}) : super(key: key);
 
+  final options = const ['One', 'Two', 'Three', 'Four', 'Five'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,19 +12,45 @@ class HomePageTemp extends StatelessWidget {
           title: const Text('Components Temp'),
         ),
         body: ListView(
-          children: const <Widget>[
-            ListTile(
-              title: Text('List Title'),
-            ),
-            Divider(),
-            ListTile(
-              title: Text('List Title'),
-            ),
-            Divider(),
-            ListTile(
-              title: Text('List Title'),
-            ),
-          ],
+          children: _createShortItems(),
         ));
+  }
+
+  List<Widget> _createItems() {
+    List<Widget> list = <Widget>[];
+
+    for (String opt in options) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      list
+        ..add(tempWidget)
+        ..add(const Divider());
+    }
+
+    return list;
+  }
+
+  List<Widget> _createShortItems() {
+    return options.map((option) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text('$option!'),
+            subtitle: const Text('Cualquier cosa'),
+            leading: const SizedBox(
+              child: Icon(Icons.access_time),
+              height: double.maxFinite, // * Center leading Icon
+            ),
+            trailing: const Icon(Icons.keyboard_arrow_right_outlined),
+            onTap: () => {},
+          ),
+          const Divider(
+            height: 1,
+          ),
+        ],
+      );
+    }).toList();
   }
 }
