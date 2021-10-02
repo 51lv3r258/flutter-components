1f86a06d263a3b7750c0bcaff3f64287385f1917
51lv3r258 <jrp8900@gmail.com>
2021-10-02 12:58:33 -0500

    ✨ Adding an alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,11 +9,49 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Alert Page'),
       ),
-      body: Container(),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: Colors.red, shape: const StadiumBorder()),
+          child: const Text('Mostrar Alerta'),
+          onPressed: () => _showAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.keyboard_return_outlined),
         onPressed: () => Navigator.pop(context),
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true, // * Close on click to exterior to the card
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: const Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const <Widget>[
+              Text('Este es el contenido de la caja de la alerta'),
+              FlutterLogo(
+                size: 100,
+              )
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Cancelar')),
+            TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Ok')),
+          ],
+        );
+      },
     );
   }
 }
