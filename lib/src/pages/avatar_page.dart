fcf9447730f9ce9e49784af941e0d5128760f1a3
51lv3r258 <jrp8900@gmail.com>
2021-10-02 20:36:09 -0500

    ✨ Using circle avatars and some of FadeInImage

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class AvatarPage extends StatelessWidget {
   const AvatarPage({Key? key}) : super(key: key);
 
@@ -9,8 +8,32 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(5.0),
+            child: const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://ichef.bbci.co.uk/news/640/cpsprodpb/A85B/production/_104299034_tv050571340.jpg'),
+              radius: 25,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(right: 20),
+            child: const CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
-      body: Container(),
+      body: const Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage(
+              'https://media.wired.com/photos/5be9d68a5d7c6a7b81d79e25/master/pass/StanLee-610719480.jpg'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
     );
   }
 }
